7c860633964d364086a6e5779b1961aa94da764b
Tanya Jurecki <tljurecki@gmail.com>
2022-05-27 13:33:58 -0400

    installed dependencies and added db set up and connection

---
+++ server.js
@@ -0,0 +1,38 @@
+//import dependencies
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const session = require('connect-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'banana',
+    cookie: {},
+    resave: true,
+    rolling: true,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    }) 
+};
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+
+//turn on routes
+app.use(routes);
+
+//turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
